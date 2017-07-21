8c03f540e9b3880b522891d60fea1d44d69fbbb7
Bel-R-Corman <belrcorman@gmail.com>
2017-07-21 09:50:21 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+print("Hello World")
+for skips in range (6):
+    
+    print(skips)
---
+++ menu.py
@@ -0,0 +1,3 @@
+main = ["Bear Can Chicken", "Lamb Chops", "Souse Vide Duck", "Tagine", "Roast"]
+side = ["Plum pudding", "Fench Fries", "Onion rings", "Rice", "Glop"]
+dessert = ["Chocolate Mousse", "Lemon Sorbet" "Deep Fried Oreos"]
